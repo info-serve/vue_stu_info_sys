3e88058750e25929632238c0cdf63686d2b1b847
info-serve <liusy.gx@aliyun.com>
2019-12-18 21:31:07 +0800

    url

---
+++ URLs.js
@@ -0,0 +1,11 @@
+const URLs = {
+    baseURL:'http://api.duyiedu.com',
+    findByPage: '/api/student/findByPage',
+    delBySno: '/api/student/delBySno',
+    updateStu: '/api/student/updateStudent',
+    addStudent:'/api/student/addStudent',
+    stuSearch: '/api/student/searchStudent',
+    findAll:'/api/student/findAll'
+
+}
+export default URLs ;
